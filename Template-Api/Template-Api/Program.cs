e317c6158fecc5e94e60849d1f6bc5d74def2c8b
facumonayar <facumonayar6@gmail.com>
2025-05-30 00:00:53 -0300

    Add Startup
    Se reconfigura API para trabajar con middleware de Startup

--- Template-Api/Template-Api/Program.cs
+++ Template-Api/Template-Api/Program.cs
@@ -1,36 +1,18 @@
 
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Template_Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(confing  => { confing.UseStartup<Startup>(); });
         }
     }
 }
